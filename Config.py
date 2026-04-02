b10598166c783ac02cd22cf7d4bacea56a8c0002
Avoooma <aikk2000ess@gmail.com>
2026-04-02 19:59:01 +0300

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# Список Telegram ID адміністраторів (додай свій)
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
